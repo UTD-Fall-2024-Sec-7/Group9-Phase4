e2b991544547e5f446c07b749f7a276e3d1a26e3
shrimoy23 <103551208+shrimoy23@users.noreply.github.com>
2024-09-06 15:21:53 -0500

    Shrimoy Satpathy, uploaded file

---
+++ IDedObject.java
@@ -0,0 +1,13 @@
+//Ameya Sansguiri
+//axs210094
+
+//This interface contains many methods that are implemented in the Product class.
+public interface IDedObject {
+public int getID();		//Returns the id
+public String getProduct();		//Returns the name of the product
+public String getSupplier();		//Returns the name of the supplier
+public void printID();		//Prints the ID
+
+
+
+}
