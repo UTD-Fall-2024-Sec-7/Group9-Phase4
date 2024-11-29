5a8f8866e5d078d5dddc4e5859c0406a0686a73b
as867 <141188786+as867@users.noreply.github.com>
2024-11-29 15:50:18 -0600

    Updated App.js

--- walletwatch_front_end/src/App.js
+++ walletwatch_front_end/src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
 import CreateAccount from './components/CreateAccount';
+import Dashboard from './components/Dashboard';
+import ViewBudgets from './components/ViewBudgets';
+import AddBudget from './components/AddBudget';
+import EditBudget from './components/EditBudget';
+import DeleteBudget from './components/DeleteBudget';
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -13,6 +19,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="createAccount" element={<CreateAccount />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="viewBudgets" element={<ViewBudgets />} />
+        <Route path="addBudget" element={<AddBudget />} />
+        <Route path="editBudget" element={<EditBudget />} />
+        <Route path="deleteBudget" element={<DeleteBudget />} />
       </Routes>
       </Router>
     </div>
