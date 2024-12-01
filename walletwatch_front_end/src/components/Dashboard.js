fcf0b870aa8c294c7cc8646e67ddc789dde596f1
Model9000 <WAR230000@utdallas.edu>
2024-11-30 23:14:05 -0600

    Revert "Merge branch 'Flask-Port' of https://github.com/UTD-Fall-2024-Sec-7/Group9-Phase4 into Flask-Port"
    This reverts commit de7b93b4272f1ed7fd5d92afab27f2cb30e85752, reversing
    changes made to e6c9a82a5a5cc699a61e43dca072309309c58ae7.

--- walletwatch_front_end/src/components/Dashboard.js
+++ walletwatch_front_end/src/components/Dashboard.js
@@ -5,6 +5,7 @@ import './Home.css';
 import HelpIcon from '@mui/icons-material/Help';
 import Stack from '@mui/material/Stack';
 
+
 function Dashboard() {
     const [user, setUser] = useState([]);
 
@@ -35,13 +36,13 @@ function Dashboard() {
        </Button>
        
             <h1> WalletWatch</h1>
-<Link to="/"><Button style ={{color: 'white', backgroundColor:'black', height:30}}>
+<Link to="/login"><Button style ={{color: 'white', backgroundColor:'black', height:30}}>
         <p>Logout</p>
      </Button></Link>
             </Stack>
      </header>
-     <h2 style={{ textAlign: 'center', fontFamily: 'Arial'}}> Welcome {user.firstName}!</h2>
-     <p style={{ textAlign: 'center', fontFamily: 'Arial'}}>
+     <h2 style={{ textAlign: 'center' }}> Welcome {user.firstName}!</h2>
+     <p style={{ textAlign: 'center' }}>
     <h3 style={{textAlign:'center'}}> What would you like to do today?</h3>
         </p>
      <footer classname = "footer">
--- walletwatch_front_end/src/components/Home.js
+++ walletwatch_front_end/src/components/Home.js
@@ -1,31 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import './Home.css';
 import HelpIcon from '@mui/icons-material/Help';
 import Stack from '@mui/material/Stack';
 
+
 function Home() {
-
-    useEffect(() => {
-        logoutUser();
-    }, []);
-
-    const logoutUser = async (e) => {
-        try {
-            const response = await fetch('/api/logout')
-            const data = await response.json();
-            if (!response.ok) {
-                alert(data.error);
-                return;
-            }
-            console.log(data);
-        } catch (error) {
-            console.error(error);
-            alert('An error occurred during login. Please try again.');
-        }
-    };
-
     return (
         <div classname = "Home">
             <header classname = "header" >
@@ -43,10 +24,11 @@ function Home() {
      </Button></Link>
             </Stack>
      </header>
-     <h2 style={{ textAlign: 'center', fontFamily: 'Arial'}}> Welcome to WalletWatch!</h2>
-     <p style={{ textAlign: 'center', fontFamily: 'Arial' }}>
+     <h2 style={{ textAlign: 'center' }}> Welcome to WalletWatch!</h2>
+     <p style={{ textAlign: 'center' }}>
      Your personal budget management app <br></br><br></br>
       Get started today! <br></br><br></br>
+ 
      <Link to="/createAccount"><Button style ={{color: 'white', backgroundColor:'black', height:30}}>
         <p>Create Account</p>
      </Button></Link>    </p>
